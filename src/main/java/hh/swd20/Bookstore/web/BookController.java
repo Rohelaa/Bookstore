ff743a748eb0d9cfeb3765d97d18851cb24a69c3
Roope Laakso <roope.laakso@gmail.com>
2019-09-10 15:14:49 +0300

    crud repo yms.

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,13 +1,33 @@
 package hh.swd20.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import hh.swd20.Bookstore.domain.Book;
+import hh.swd20.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
+	@Autowired
+	private BookRepository bookRepository;
+	
 	@RequestMapping("/index")
 	public String getIndex() {
 		return "index";
+	}
+	
+	@RequestMapping("/booklist")
+	public String getBooks(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "booklist";
+	}
+	
+	@RequestMapping("/addbook")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
 	}
 }
